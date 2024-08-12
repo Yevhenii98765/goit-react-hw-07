feea44c00805242b612c688db42c4ed5047e1371
Kenediii <156183790+Kenediii@users.noreply.github.com>
2024-08-12 15:46:42 +0300

    fix

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -1,7 +1,7 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import { useDispatch } from "react-redux";
 import * as Yup from "yup";
-import { addContactThunk } from "../../redux/Contacts/operations";
+import { addContactThunk } from "../../redux/Contacts/contactsOps";
 const initialValues = {
   name: "",
   number: "",
